88362091ecde9deb35dfedb84d10476bdcd7d390
Deepak Jain <jaindeepak1401+jym@gmail.com>
2026-03-14 09:53:20 +0530

    add healthz checkpoint

--- srv/server.js
+++ srv/server.js
@@ -1,3 +1,10 @@
 require('dotenv').config();
 const cds = require('@sap/cds');
+
+cds.on('bootstrap', (app) => {
+  app.get('/healthz', (_req, res) => {
+    res.status(200).json({ status: 'ok', timestamp: new Date().toISOString() });
+  });
+});
+
 module.exports = cds.server;
